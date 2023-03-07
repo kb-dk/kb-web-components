754e7090b79b99c5d9d066b6b145b64dcd473d1b
zahramousavi <zahramousavidk@gmail.com>
2023-03-07 14:02:53 +0100

    Fixed kb-style component imports.

--- kb-style/index.js
+++ kb-style/index.js
@@ -1,6 +1,6 @@
 import {css} from "lit";
-import {BSColLG3, BSColSM6, BSContainer, BSH3, BSRow} from "kb-bootstrap.js"
-import {KBColors} from "kb-base.js"
+import {BSColLG3, BSColSM6, BSContainer, BSH3, BSRow} from "@kb-dk/kb-style/kb-bootstrap.js"
+import {KBColors} from "@kb-dk/kb-style/kb-base.js"
 
 export {BSColLG3, BSColSM6, BSContainer, BSH3, BSRow};
 export {KBColors};
